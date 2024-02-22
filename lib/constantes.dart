9de4e804cbf2f7b31734ed947b7306d6c8865786
Lucas09M <lucas.miralles09@gmail.com>
2024-02-22 16:05:56 +0100

    Fondo imagen1

---
+++ lib/constantes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const fondo = Color(0xff520101);
+const fondo2 = Color(0xff640101);
+const fondo3 = Color(0xff410101);
+const titulos = Color(0xff940043);
+const botones = Color(0xffdc0000);
+const efectos = Color(0xff9b0202);
--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:examen_1_lmbm/constantes.dart' as cons;
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,14 +12,46 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery
-        .of(context)
-        .size;
+    final size = MediaQuery.of(context).size;
+    final screenHeight = size.height;
+
     return Scaffold(
-        body: Stack(
-        )
+      backgroundColor: cons.fondo,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _buildRowWithRectangles(screenHeight, cons.fondo2, cons.fondo, cons.fondo3),
+          _buildRowWithRectangles(screenHeight, cons.fondo3, cons.fondo2, cons.fondo),
+          _buildRowWithRectangles(screenHeight, cons.fondo, cons.fondo3, cons.fondo2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRowWithRectangles(double screenHeight, Color color1, Color color2, Color color3) {
+    final rectangleHeight = screenHeight / 3;
+
+    return Container(
+      height: rectangleHeight,
+      child: Row(
+        children: [
+          _buildRectangle(color1, screenHeight / 3),
+          _buildRectangle(color2, screenHeight / 3),
+          _buildRectangle(color3, screenHeight / 3),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRectangle(Color color, double height) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20), // Bords un peu ronds
+        ),
+        height: height,
+      ),
     );
   }
 }
-
-
