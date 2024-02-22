bdb3f7f8db2faec982ab358660167617afede817
Lucas09M <lucas.miralles09@gmail.com>
2024-02-22 18:53:54 +0100

    Final imagen1

---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:examen_1_lmbm/constantes.dart' as cons;
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery
+        .of(context)
+        .size;
+    return Scaffold(
+    );
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:examen_1_lmbm/constantes.dart' as cons;
+import 'home.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -9,6 +10,10 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool bandera = false;
+  bool campo = false;
+  final user = TextEditingController();
+  final pass = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +22,131 @@ class _LoginState extends State<Login> {
 
     return Scaffold(
       backgroundColor: cons.fondo,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
         children: [
-          _buildRowWithRectangles(screenHeight, cons.fondo2, cons.fondo, cons.fondo3),
-          _buildRowWithRectangles(screenHeight, cons.fondo3, cons.fondo2, cons.fondo),
-          _buildRowWithRectangles(screenHeight, cons.fondo, cons.fondo3, cons.fondo2),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildRowWithRectangles(screenHeight, cons.fondo2, cons.fondo, cons.fondo3),
+              _buildRowWithRectangles(screenHeight, cons.fondo3, cons.fondo2, cons.fondo),
+              _buildRowWithRectangles(screenHeight, cons.fondo, cons.fondo3, cons.fondo2),
+            ],
+          ),
+          Center(
+            child: Container(
+              width: size.width * 0.8,
+          /*    height: screenHeight * 0.5,*/
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.7),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10), // Ajoute du padding vertical
+                    child: Text(
+                      'Bienvenido a tu primer EXAMEN',
+                      style: TextStyle(
+                        color: cons.titulos,
+                        fontSize: 21,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: user,
+                    decoration: InputDecoration(
+                      hintText: 'Correo/Usuario',
+                      filled: true, // Ajoute un fond rempli blanc
+                      fillColor: Colors.white, // Couleur de fond blanc
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: pass,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Contraseña',
+                      filled: true, // Ajoute un fond rempli blanc
+                      fillColor: Colors.white, // Couleur de fond blanc
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        campo = false; // Réinitialise la variable campo à false
+                        bandera = false; // Réinitialise la variable bandera à false
+
+                        if (user.text.isEmpty || pass.text.isEmpty) {
+                          // Vérifie si l'un des champs est vide
+                          campo = true;
+                        } else if (user.text == 'admin' && pass.text == '123456') {
+                          // Si les identifiants et les mots de passe correspondent, naviguer vers une autre page
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
+                        } else {
+                          // Si les identifiants et les mots de passe ne correspondent pas, définir une variable "bandera" sur true
+                          bandera = true;
+                        }
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: cons.botones,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      fixedSize: Size(size.width * 0.6, 40),
+                    ),
+                    child: Text(
+                      'Iniciar Sesión',
+                      style: TextStyle(color: Colors.white, fontSize: 16,),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  bandera
+                      ? const Text(
+                    'Usuario o contraseña incorrectos',
+                    style: TextStyle(color: cons.efectos),
+                    textAlign: TextAlign.center,
+                  )
+                      : const SizedBox(),
+                  campo
+                      ? const Text(
+                    'Por favor, rellene todos los campos',
+                    style: TextStyle(color: cons.efectos),
+                    textAlign: TextAlign.center,
+                  )
+                      : const SizedBox(),
+                  SizedBox(height: 10),
+                  Text(
+                    'Mi primer examen, ¿estará sencillo?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -48,7 +172,7 @@ class _LoginState extends State<Login> {
       child: Container(
         decoration: BoxDecoration(
           color: color,
-          borderRadius: BorderRadius.circular(20), // Bords un peu ronds
+          borderRadius: BorderRadius.circular(20),
         ),
         height: height,
       ),
